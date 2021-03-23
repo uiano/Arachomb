7ba55b95c3a3025f4a8a8347012aa9f2d8adce44
sebasa18 <sebasa18@uia.no>
2021-03-23 16:44:39 +0100

    Removed mention of inserting the relevant part of the code

--- cli.py
+++ cli.py
@@ -14,7 +14,7 @@ cur = con.cursor()
 print("finding stuff")
 for error, source, target, timestamp in cur.execute(f"SELECT error_code,source,target,updated_at FROM errors ORDER BY source").fetchall():
     print(f"""*****************\nWe found an error in {source}, in the link to 
-        {target}:\n\nhot diggity code here\n\n
+        {target}\n\n
         Getting the link returned a {error} error, suggesting you should 
         {suggestion(error)}.\n\n
         Last checked at {timestamp}""")
