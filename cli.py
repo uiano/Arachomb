92e5a2e1947d496bd4d63d07e68735a1fb403fde
sebasa18 <sebasa18@uia.no>
2021-03-29 14:26:19 +0200

    Added a handful of options to the suggestion function

--- cli.py
+++ cli.py
@@ -6,9 +6,13 @@ import sqlite3
 #domains = set(filter(lambda x: data[x], data.keys()))
 
 def suggestion(code):
-    #TODO: do something with this?
-    if code == "404": pass
-    return code
+    if code == "404":
+        return "make sure the URL is spelled correctly, and that the resource exists."
+    elif code == "403":
+        return "make sure the resource is publically available.  If this is intentional, ignore this error."
+    elif code == "405":
+        return "double check that the URL is spelled correctly, as the page only allows certain non-GET requests, so it won't appear properly in a browser."
+    return "figure out what kind of error this is, because we do not know."
 
 con = sqlite3.connect("data.db")
 cur = con.cursor()
@@ -17,5 +21,5 @@ for error, source, target, timestamp in cur.execute(f"SELECT error, source, targ
     print(f"""*****************\nWe found an error in {source}, in the link to 
         {target}\n\n
         Getting the link returned a {error} error, suggesting you should 
-        {suggestion(error)}.\n\n
+        {suggestion(error)}\n\n
         Last checked at {timestamp}""")
